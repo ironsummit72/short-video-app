1d530bb277b29e98ea9929ab2ce67a936244fd8f
ironsummit72 <ironsumit72@gmail.com>
2025-01-15 13:22:16 +0530

    validation added removed dist

--- backend/dist/routes/upload.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const upload_controller_1 = require("../controller/upload.controller");
-const router = (0, express_1.Router)();
-router.post("/video", upload_controller_1.postHandleUploadVideo);
-exports.default = router;
